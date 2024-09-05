1f292aa055bcf0260bfc4744e41cea01141b9032
Gempur Bayu <gempur@rekadia.co.id>
2024-09-06 00:16:43 +0700

    feat: add model

--- src/model/accountModel.ts
+++ src/model/accountModel.ts
@@ -2,8 +2,6 @@ export interface IAccount {
   displayName: string;
   email: string;
   id: string;
-  organizationId: string;
-  organizationName: string;
   photo: string;
   roles: string[];
   username: string;
--- src/model/loginModel.ts
+++ src/model/loginModel.ts
@@ -1,5 +1,5 @@
 export interface ILoginPayload {
-  emailOrUsername: string;
+  email: string;
   password: string;
 }
 
